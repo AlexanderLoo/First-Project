01ff410279841cf55f8e8cdbd96d9e6a5d0006f6
AlexanderLoo <AlexanderLoo>
2017-09-06 23:46:51 -0500

    PlayerMovement
    -player simple movement

--- Vint/Assets/PlayerController.cs
+++ Vint/Assets/PlayerController.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	private SpriteRenderer _sr;
+	private Rigidbody2D _rb;
 	private Transform crosshair;
+
+	public float speed;
 
 	void Start(){
 
 		_sr = GetComponent<SpriteRenderer> ();
+		_rb = GetComponent<Rigidbody2D> ();
 		crosshair = GameObject.Find ("Crosshair").transform;
 	}
 
 	void Update(){
 
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+		PlayerMovement (h,v);
 		LookAtCrosshair ();
 	}
 
@@ -26,5 +33,12 @@ public class PlayerController : MonoBehaviour {
 		if (crosshair.position.x > 0) {
 			_sr.flipX = false;
 		}
+	}
+
+	void PlayerMovement(float h, float v){
+
+		Vector2 movement = new Vector2 (h, v);
+		movement.Normalize ();
+		_rb.velocity = movement * speed;
 	}
 }
