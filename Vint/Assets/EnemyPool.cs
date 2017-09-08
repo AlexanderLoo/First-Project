ff3dc8f71926de9bf0b853959e59df004c364473
AlexanderLoo <AlexanderLoo>
2017-09-08 02:20:50 -0500

    EnemySplitFuntion
    1 Bug detected:
    -BigSizeEnemy
    (el enemigo de tamaño grande no se divide)

---
+++ Vint/Assets/EnemyPool.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPool : MonoBehaviour {
+
+	//Esta lista contiene los tipos de enemigos(según tamaño o raza)
+	public GameObject[] enemiesTypes;
+	//Lista total de enemigos en EnemyPool
+	[System.NonSerialized]
+	public GameObject[] enemyList;
+
+	void Awake(){
+
+		enemyList = new GameObject[30];
+	}
+	void Start(){
+		
+		//Por cada tipo de enemigo en la lista "enemiesTypes" agregamos una cantidad a nuestro EnemyPool
+		foreach (GameObject enemyType in enemiesTypes) {
+			for (int i = 0; i < enemyList.Length/3; i++) {
+
+				GameObject newEnemy = (GameObject)Instantiate (enemyType);
+				newEnemy.SetActive (false);
+				enemyList [i] = newEnemy;
+			}
+		}
+	}
+}
--- Vint/Assets/Scripts/EnemyGetShot.cs
+++ Vint/Assets/Scripts/EnemyGetShot.cs
@@ -4,5 +4,51 @@ using UnityEngine;
 
 public class EnemyGetShot : MonoBehaviour {
 
+	//Declaramos una variable para tener acceso a la lista enemylist;
+	private GameObject[] enemyPoolList;
+	public bool bigSizeEnemy, normalSizeEnemy, smallSizeEnemy;
 
+	void Awake(){
+
+		enemyPoolList = GameObject.Find ("EnemyPool").GetComponent<EnemyPool> ().enemyList;
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		//Por el momento usamos el tag "Player" para detectar la bala
+		if (other.CompareTag("Player")) {
+			EnemySplit ();
+			other.gameObject.SetActive (false);
+			gameObject.SetActive (false);
+		}
+	}
+
+	GameObject EnemySplit(){
+
+		//Si el enemigo es de tamaño grande..recorremos la lista de enemigos y buscamos un enemigo de tamaño mediano y que este inactivo
+		//Luego llamamos la función para activar el enemigo y retornamos el enemigo
+		if (bigSizeEnemy) {
+			for (int i = 0; i < enemyPoolList.Length; i++) {
+				if (enemyPoolList [i].gameObject.GetComponent<EnemyGetShot> ().normalSizeEnemy && !enemyPoolList [i].activeSelf) {
+					ActiveEnemy (i);
+					return enemyPoolList [i];
+				}
+			}
+		}
+		if (normalSizeEnemy) {
+			for (int i = 0; i < enemyPoolList.Length; i++) {
+				if (enemyPoolList [i].gameObject.GetComponent<EnemyGetShot> ().smallSizeEnemy && !enemyPoolList [i].activeSelf) {
+					ActiveEnemy (i);
+					return enemyPoolList [i];
+				}
+			}
+		}
+		return null;
+	}
+
+	void ActiveEnemy(int i){
+
+		enemyPoolList [i].SetActive (true);
+		enemyPoolList [i].transform.position = transform.position;
+		enemyPoolList [i].transform.rotation = transform.rotation;
+	}
 }
