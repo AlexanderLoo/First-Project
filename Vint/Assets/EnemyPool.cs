111e510cdb7ad56b0814708f1d52af57ea9a22a0
AlexanderLoo <AlexanderLoo>
2017-09-09 21:46:38 -0500

    EnemyBigSize Bug Fixed
    -se arregló el bug que no permitia al enemigo grande dividirse..

--- Vint/Assets/EnemyPool.cs
+++ Vint/Assets/EnemyPool.cs
@@ -16,7 +16,7 @@ public class EnemyPool : MonoBehaviour {
 	}
 	void Start(){
 		
-		//Por cada tipo de enemigo en la lista "enemiesTypes" agregamos una cantidad a nuestro EnemyPool
+		/*Por cada tipo de enemigo en la lista "enemiesTypes" agregamos una cantidad a nuestro EnemyPool
 		foreach (GameObject enemyType in enemiesTypes) {
 			for (int i = 0; i < enemyList.Length/3; i++) {
 
@@ -24,6 +24,22 @@ public class EnemyPool : MonoBehaviour {
 				newEnemy.SetActive (false);
 				enemyList [i] = newEnemy;
 			}
+		}*/
+		for (int i = 0; i < 10; i++) {
+			GameObject newEnemy = (GameObject)Instantiate (enemiesTypes [0]);
+			newEnemy.SetActive (false);
+			enemyList [i] = newEnemy;
 		}
+		for (int i = 10; i < 20; i++) {
+			GameObject newEnemy = (GameObject)Instantiate (enemiesTypes [1]);
+			newEnemy.SetActive (false);
+			enemyList [i] = newEnemy;
+		}
+		for (int i = 20; i < 30; i++) {
+			GameObject newEnemy = (GameObject)Instantiate (enemiesTypes [2]);
+			newEnemy.SetActive (false);
+			enemyList [i] = newEnemy;
+		}
+
 	}
 }
--- Vint/Assets/Scripts/EnemyGetShot.cs
+++ Vint/Assets/Scripts/EnemyGetShot.cs
@@ -6,6 +6,8 @@ public class EnemyGetShot : MonoBehaviour {
 
 	//Declaramos una variable para tener acceso a la lista enemylist;
 	private GameObject[] enemyPoolList;
+	//Esta variable sirve para setear la cantidad de divisiones que va tener el enemigo
+	private int enemiesNum = 0, maxEnemiesNum = 2;
 	public bool bigSizeEnemy, normalSizeEnemy, smallSizeEnemy;
 
 	void Awake(){
@@ -24,13 +26,20 @@ public class EnemyGetShot : MonoBehaviour {
 
 	GameObject EnemySplit(){
 
+
 		//Si el enemigo es de tamaño grande..recorremos la lista de enemigos y buscamos un enemigo de tamaño mediano y que este inactivo
 		//Luego llamamos la función para activar el enemigo y retornamos el enemigo
 		if (bigSizeEnemy) {
 			for (int i = 0; i < enemyPoolList.Length; i++) {
 				if (enemyPoolList [i].gameObject.GetComponent<EnemyGetShot> ().normalSizeEnemy && !enemyPoolList [i].activeSelf) {
 					ActiveEnemy (i);
-					return enemyPoolList [i];
+					//Cada vez que se llame a esta función, hacemos un conteo..
+					enemiesNum += 1;
+					//si el conteo llega a la cantidad Máxima(cantidad de enemigos que se van a crear)..reseteamos el contador e interrumpimos el bucle para que deje de buscar
+					if (enemiesNum == maxEnemiesNum) {
+						enemiesNum = 0;
+						break;
+					}
 				}
 			}
 		}
@@ -38,17 +47,22 @@ public class EnemyGetShot : MonoBehaviour {
 			for (int i = 0; i < enemyPoolList.Length; i++) {
 				if (enemyPoolList [i].gameObject.GetComponent<EnemyGetShot> ().smallSizeEnemy && !enemyPoolList [i].activeSelf) {
 					ActiveEnemy (i);
-					return enemyPoolList [i];
+					enemiesNum += 1;
+					if (enemiesNum == maxEnemiesNum) {
+						enemiesNum = 0;
+						break;
+					}
 				}
 			}
 		}
 		return null;
 	}
-
-	void ActiveEnemy(int i){
+	//Esta función activa el enemigo y lo posiciona en el transform.position
+	GameObject ActiveEnemy(int i){
 
 		enemyPoolList [i].SetActive (true);
 		enemyPoolList [i].transform.position = transform.position;
 		enemyPoolList [i].transform.rotation = transform.rotation;
+		return enemyPoolList [i];
 	}
 }
