8c4406974891d6cf34d0ec1b8f890311638c7979
ElJohinn <johann0197@gmail.com>
2017-10-23 14:49:37 -0500

    Chequeo correcto
    Los dos artstryle funcionan sin bugs

--- Vint/Assets/Scripts/GameController/ArtController.cs
+++ Vint/Assets/Scripts/GameController/ArtController.cs
@@ -8,13 +8,13 @@ public class ArtController : MonoBehaviour {
 	public ArtStyles[] artStyles;
 	public int currentArtStye;
 
-	private EnemyPool enemyPool;
+	private EnemyPool enemyPool;	
 	private BulletsPool bulletPool;
 
 	void Awake(){
 
 		//Obtemos el índice del último diseño equipado
-		currentArtStye = PlayerPrefs.GetInt ("CurrentArtStyle");
+		//currentArtStye = PlayerPrefs.GetInt ("CurrentArtStyle");
 		//Instanciamos al player según el diseño obtenido
 		Instantiate (artStyles [currentArtStye].player, Vector2.zero, Quaternion.identity);
 		enemyPool = GameObject.Find ("EnemyPool").GetComponent<EnemyPool>();
