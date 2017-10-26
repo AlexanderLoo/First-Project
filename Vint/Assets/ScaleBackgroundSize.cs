ce7b5072fe0d4ccde91011ad5d43c1b92a0e74d1
AlexanderLoo <AlexanderLoo>
2017-10-26 17:31:08 -0500

    Background Script
    -Se añadio el script que permite escalar el background para ajustarse a la pantalla

--- Vint/Assets/ScaleBackgroundSize.cs
+++ Vint/Assets/ScaleBackgroundSize.cs
@@ -7,7 +7,33 @@ public class ScaleBackgroundSize : MonoBehaviour {
 	void Start(){
 
 		Sprite spriteSize = gameObject.GetComponent<SpriteRenderer> ().sprite;
-		spriteSize.rect.size.Set (Screen.width *2, Screen.height*2);
-		print (spriteSize);
+
+		float pu = spriteSize.pixelsPerUnit;
+
+		//Transformamos las medidas en metros del tamaño mínimo y máximo en las coordenadas X y Y
+		Vector3 upperRightCorner = Camera.main.ViewportToWorldPoint (new Vector3 (1, 1, 1));
+		Vector3 lowerLeftCorner = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, 0));
+		Vector3 screenMeters = upperRightCorner - lowerLeftCorner;
+
+		float screenWithMeters = screenMeters.x;
+
+		float unityPixelPerMeter = (Screen.width / screenWithMeters);
+
+		float proportionUnityToSprite = pu / unityPixelPerMeter;
+
+		float originalX = spriteSize.rect.size.x;
+		float originalY = spriteSize.rect.size.y;
+
+		float scaleX = Screen.width / originalX;
+		float scaleY = Screen.height / originalY;
+
+		float generalProportion = 1f;
+
+		if (scaleX > scaleY) {
+			generalProportion = scaleX;
+		} else {
+			generalProportion = scaleY;
+		}
+		transform.localScale = Vector3.one * (generalProportion * proportionUnityToSprite);
 	}
 }
