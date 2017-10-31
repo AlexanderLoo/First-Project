101462b74e8da620d03b0026d962fa8f521cd375
ElJohinn <johann0197@gmail.com>
2017-10-30 20:24:35 -0500

    Coin Blinking
    - Las monedas parpadean despues de 2 segundos

--- Vint/Assets/Scripts/Resources/Coin.cs
+++ Vint/Assets/Scripts/Resources/Coin.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class Coin : MonoBehaviour {
 
 	public float destroyTime;
+	private SpriteRenderer _spriteRenderer;
+	private float targetAlpha;
+	private bool StartBlinking = false;
 
 	void OnEnable(){
 		//Hacemos desaparecer la moneda después de un breve periodo de tiempo
+		_spriteRenderer = GetComponent<SpriteRenderer> ();
+		Invoke ("Blink", 2);
 		Invoke ("DestroyCoin",destroyTime);
+
+	}
+	void Update(){
+		ManageBlinking ();
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
@@ -23,9 +32,37 @@ public class Coin : MonoBehaviour {
 	void DestroyCoin(){
 
 		gameObject.SetActive (false);
+		StartBlinking = false;
+
+
+	}
+	void Blink(){
+		StartBlinking = true;	
 	}
 
 	void OnDisable(){
 		CancelInvoke ("DestroyCoin");
+		CancelInvoke ("Blink");
+
+		
+	}
+
+	void ManageBlinking (){
+		if (StartBlinking) {
+			Color newColor = _spriteRenderer.color;
+			newColor.a = Mathf.Lerp (newColor.a, targetAlpha, Time.deltaTime * 20);
+			if (newColor.a < 0.05f) {
+				targetAlpha = 1;
+			}
+			if (newColor.a > 0.95f) {
+				targetAlpha = 0;
+			}
+			_spriteRenderer.color = newColor;
+	
+		} else {
+			Color newColor = _spriteRenderer.color;
+			newColor.a = 1; 
+			_spriteRenderer.color = newColor;
+		}
 	}
 }
